ecce97d337b65af9525c62feb661096cc80c2ada
Meplos <alexxd64150@gmail.com>
2021-08-08 15:57:55 +0200

    rapport: vue

---
+++ assets/code/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>
+      {{ helloworld }}
+    </h1>
+    <button @click="count">CLICK ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  components: {},
+  mixins: [],
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      hello: "Hello",
+      count: 0,
+    };
+  },
+  computed: {
+    helloworld() {
+      return this.hello + " " + this.msg + "!";
+    },
+  },
+  methods: {
+    count: function () {
+      this.count++;
+    },
+  },
+  mounted() {
+    console.log("HelloWorld component mounted");
+  },
+};
+</script>
+
+<style></style>
